5aa08d93232380d4b59450abf790ce9e64c78c8a
unonewcreative <unonewcreative@gmail.com>
2021-06-17 13:47:19 +0200

    del

--- 05-python-novice/test.py
+++
@@ -1,9 +0,0 @@
-getal = int(input("Tot welk getal?: "))
-
-# tel op tot getal
-for i in range(1, getal + 1):
-    print(i)
-
-# tel terug van getal
-for i in reversed(range(1, getal)):
-    print(i)
